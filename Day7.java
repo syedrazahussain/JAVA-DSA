a95f19ccb72ec39447ee95570a5ef89694233bd1
syedrazahussain <srazahussain1514@gmail.com>
2026-06-16 19:18:56 +0530

    Day 7 addition amd  multiplicaftion  problem practised

---
+++ Day7.java
@@ -0,0 +1,16 @@
+//=====================(day -7 2d array sum and multiplication problem)=========
+
+import java.util.Scanner;
+
+class Day7{
+
+    static void add(int[][] a,int[][]b,int r1,int c1,int r2,int c2){
+        System.out.println("Enter the size of the matrix 1");
+        Scanner sc = new Scanner(System.in)
+    }
+    public static void main(String[] args) {
+        // question 1 : addtion of the 2 matrices
+        //note only matrices with same dimension 
+
+    }
+}
