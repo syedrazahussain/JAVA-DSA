871b0ab7eb39ffb5f0f23889b5a6a66f3a4ff803
syedrazahussain <srazahussain1514@gmail.com>
2026-06-25 18:06:54 +0530

    Day8 transpose and pascal traingle problem practised

---
+++ Day8.java
@@ -0,0 +1,55 @@
+//========================Day8(transpose of matrix,pascal's traingle)=============
+
+import java.util.Scanner;
+
+class Day8{
+    static void printMatrix(int matrix[][], int r,int c){
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < matrix[i].length; j++ ){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return;
+    }
+    static int[][] transposeMatrix(int matrix[][],int c,int r){
+        
+        int ans[][] = new int[c][r];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+         
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Row no of matrix ");
+        int r = sc.nextInt();
+        System.out.println("Enter the Column no of matrix ");
+        int c = sc.nextInt();
+        int matrix[][] = new int[r][c];
+        System.out.println("Enter matrix " + r*c + " Elements : ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+            
+                matrix[i][j] = sc.nextInt();
+                
+                
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Input matrix");
+        
+        printMatrix(matrix, r, c);
+        System.out.println("Transpose matrix is : ");
+        int ans[][] = transposeMatrix(matrix, c, r);
+        printMatrix(ans, c, r);
+
+
+     
+        
+
+    }
+}
