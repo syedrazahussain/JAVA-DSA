78cb749c53f911024c0a3b1529871b9a25ba019e
syedrazahussain <srazahussain1514@gmail.com>
2026-06-27 16:14:25 +0530

    Day8 transpose and pascal traingle problem practised

--- Day8.java
+++ Day8.java
@@ -25,16 +25,41 @@ class Day8 {
         return ans;
     }
 
-    static int[][] transposeinplacematrix(int matrix[][],int c,int r){
-        for(int i = 0; i < r; i++){
-            for(int j = i; j < c; j++){
-                int temp= matrix[i][j];
+    static int[][] transposeinplacematrix(int matrix[][], int c, int r) {
+        for (int i = 0; i < r; i++) {
+            for (int j = i; j < c; j++) {
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
         return matrix;
+    }
+
+    static void pascalsTraingle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row no : ");
+        int n = sc.nextInt();
+        int mat[][] = new int[n][];
+        for (int i = 0; i < n; i++) {
+            mat[i] = new int[i + 1];
+            mat[i][0] = 1;
+            mat[i][i] = 1;
+
+            for (int j = 1; j < i ; j++) {
+
+                mat[i][j] = mat[i - 1][j] + mat[i - 1][j - 1];
+                
+               
+            }
+            
+            
+        }
+       
+        printMatrix(mat, n, n);
+
+        return;
     }
 
     public static void main(String[] args) {
@@ -46,27 +71,47 @@ class Day8 {
         int matrix[][] = new int[r][c];
         System.out.println("Enter matrix " + r * c + " Elements : ");
         for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+        for (int j = 0; j < c; j++) {
 
-                matrix[i][j] = sc.nextInt();
+        matrix[i][j] = sc.nextInt();
 
-            }
-            System.out.println(" ");
+        }
+        System.out.println(" ");
         }
         System.out.println("Input matrix");
 
         printMatrix(matrix, r, c);
 
         // question 1 : write a program to display transpose matrix entered by the user
-        // System.out.println("Transpose matrix is : ");
-        // int ans[][] = transposeMatrix(matrix, c, r);
-        // printMatrix(ans, c, r);
+        System.out.println("Transpose matrix is : ");
+        int ans[][] = transposeMatrix(matrix, c, r);
+        printMatrix(ans, c, r);
 
         // question 2 : transpose array in place without creating new array
-         System.out.println("Transpose matrix is : ");
-        //  int matrix1[][] = transposeMatrix(matrix, c, r);
-         transposeinplacematrix(matrix, r, c);
-         printMatrix(matrix, r, c);
-         
+
+        System.out.println("Transpose matrix is : ");
+        transposeinplacematrix(matrix, r, c);
+        printMatrix(matrix, r, c);
+
+        // question 3 : given an intrger n ,return the first n rows of pascal's traingle
+        // ,each nuumber is the sum
+        // of the two numbers directly above it as shown for n = 5
+
+        // example figure : if(3,1) => then adding of above element that is 2 + 1 = 3
+        // if(4,2) => then adding of above element that is 3 + 3 = 6
+
+        // 0
+
+        // 0 1 //1
+
+        // 1 1 1 //2
+
+        // 2 1 2 1 //3
+
+        // 3 1 3 3 1 //4
+
+        // 4 1 4 6 4 1
+        pascalsTraingle();
+
     }
 }
